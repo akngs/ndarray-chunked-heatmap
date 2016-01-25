62d69f0d7cb3f37e2ac953d8d7e78b8b8806c092
akngs <jania902@gmail.com>
2016-01-25 21:33:33 +0900

    Remove invalid test code

--- test/unit/ndarray-chunked-heatmap.js
+++ test/unit/ndarray-chunked-heatmap.js
@@ -8,23 +8,5 @@ describe('cheatmap', () => {
   });
 
   it('should do something', () => {
-    chm
-      .chunkShape([3, 3])
-      .data([
-          [1, 2, 3, 4, 5, 6, 7],
-          [1, 2, 3, 4, 5, 6, 7],
-          [1, 2, 3, 4, 5, 6, 7],
-          [1, 2, 3, 4, 5, 6, 7],
-          [1, 2, 3, 4, 5, 6, 7]
-      ]);
-    let canvases = chm();
-    let div = document.createElement('div');
-    document.body.appendChild(div);
-
-    for(let i = 0; i < canvases.length; ++i) {
-      for(let j = 0; j < canvases[i].length; ++j) {
-        div.appendChild(canvases[i][j]);
-      }
-    }
   });
 });
